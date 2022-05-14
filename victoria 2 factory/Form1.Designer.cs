1544ed29408c970ebdb09e802d1b06152c0b1138
PacTi <ibrahimyandi@outlook.com>
2022-05-14 13:52:05 +0300

    v1.0

---
+++ victoria 2 factory/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace victoria_2_factory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(496, 11);
+            this.listView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(498, 355);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.Click += new System.EventHandler(this.listView1_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(496, 370);
+            this.listView2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(274, 306);
+            this.listView2.TabIndex = 3;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(496, 680);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(274, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(15, 49);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(255, 660);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 14);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Search state:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(276, 42);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(214, 116);
+            this.listBox2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(277, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "RGO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(276, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Factories";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(276, 209);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(214, 500);
+            this.listBox3.TabIndex = 11;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 16;
+            this.listBox4.Location = new System.Drawing.Point(776, 370);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(218, 340);
+            this.listBox4.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 721);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Factory Management";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+    }
+}
+
---
+++ victoria 2 factory/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace victoria_2_factory
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string provinceJson = File.ReadAllText(Application.StartupPath + "\\province.json");
+            var province = JsonConvert.DeserializeObject<List<province>>(provinceJson);
+
+            foreach (var item in province)
+            {
+                if (!(listBox1.Items.Contains(item.Region.ToLower())))
+                {
+                    listBox1.Items.Add(item.Region.ToLower());
+                }
+            }
+
+            listView1.View = View.LargeIcon;
+            listView1.FullRowSelect = true;
+            listView1.Columns.Add("", 150);
+            listView1.Columns.Add("", 150);
+            listView2.View = View.LargeIcon;
+            listView2.FullRowSelect = true;
+            listView2.Columns.Add("", 150);
+            listView2.Columns.Add("", 150);
+
+            string json = File.ReadAllText(Application.StartupPath + "\\rgo.json");
+            var RGO = JsonConvert.DeserializeObject<List<RGO>>(json);
+            int counter = 0;
+
+            foreach (var product in RGO)
+            {
+                imageList1.Images.Add(Image.FromFile(Application.StartupPath + @"\images\" + product.Name + ".png"));
+            }
+
+            this.imageList1.ImageSize = new Size(32, 32);
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView2.LargeImageList = this.imageList1;
+
+            foreach (var product in RGO)
+            {
+                ListViewItem item = new ListViewItem();
+                item.ImageIndex = counter;
+                listView1.Items.Add(product.Name, counter);
+
+                counter++;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string provinceJson = File.ReadAllText(Application.StartupPath + "\\province.json");
+            var province = JsonConvert.DeserializeObject<List<province>>(provinceJson);
+
+            foreach (var item in province)
+            {
+                if (item.Region.ToLower().Contains(textBox1.Text.ToLower()) && !(listBox1.Items.Contains(item.Region.ToLower())))
+                {
+                    listBox1.Items.Add(item.Region.ToLower());
+                }
+            }
+        }
+
+        private void listView1_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices.Count <= 0)
+            {
+                return;
+            }
+            int intselectedindex = listView1.SelectedIndices[0];
+            if (intselectedindex >= 0 && listView2.Items.Count != 0)
+            {
+                foreach (ListViewItem item in listView2.Items)
+                {
+                    if (item.Text == listView1.Items[intselectedindex].Text)
+                    {
+                        return;
+                    }
+                }
+                listView2.Items.Add(listView1.Items[intselectedindex].Text, intselectedindex);
+            }
+            else
+            {
+                listView2.Items.Add(listView1.Items[intselectedindex].Text, intselectedindex);
+            }
+
+
+            LW2Countrol();
+        }
+        public void LW2Countrol()
+        {
+            string json = File.ReadAllText(Application.StartupPath + "\\factories.json");
+            var factories = JsonConvert.DeserializeObject<List<Factories>>(json);
+            int intselectedindex = listView1.SelectedIndices[0];
+
+            foreach (var factory in factories)
+            {
+                foreach (var product in factory.Inputs)
+                {
+                    if (product == listView1.Items[intselectedindex].Text)
+                    {
+                        if (!listBox4.Items.Contains(listView1.Items[intselectedindex].Text) && !listBox4.Items.Contains(factory.Name))
+                        {
+                            listBox4.Items.Add(factory.Name);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView2.Items.Clear();
+            listBox4.Items.Clear();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string json = File.ReadAllText(Application.StartupPath + "\\factories.json");
+            var factories = JsonConvert.DeserializeObject<List<Factories>>(json);
+
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            string provinceJson = File.ReadAllText(Application.StartupPath + "\\province.json");
+            var province = JsonConvert.DeserializeObject<List<province>>(provinceJson);
+            string selectedRegion = listBox1.SelectedItem.ToString();
+            foreach (var item in province)
+            {
+                if (selectedRegion == item.Region.ToLower() && !listBox2.Items.Contains(item.Resource))
+                {
+                    listBox2.Items.Add(item.Resource);
+                    foreach (var factory in factories)
+                    {
+                        foreach (var product in factory.Inputs)
+                        {
+                            if (product == item.Resource && !listBox3.Items.Contains(factory.Name))
+                            {
+                                listBox3.Items.Add(factory.Name);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public class Factories
+    {
+        public string Name;
+        public List<string> Inputs;
+    }
+    public class RGO
+    {
+        public string Name;
+        public string Image;
+    }
+    public class province
+    {
+        public string Resource;
+        public string Region;
+    }
+}
